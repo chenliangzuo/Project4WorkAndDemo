b50c419c89d07b50fabf65978009ea27afdf880b
念兹在兹 <1150542038@qq.com>
2021-02-19 05:08:50 +0000

    Update ChenApplication.java
    github上提交测试

--- src/main/java/cn/chen/ChenApplication.java
+++ src/main/java/cn/chen/ChenApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChenApplication {
 
     /**
-     * 自己添加的原始Websocket，无样式，效果简单
+     * 自己添加的原始Websocket，无样式，效果简单 - 哈哈哈
      * @param args
      */
     public static void main(String[] args) {
